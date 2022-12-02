c1bc334486fe5b8f10a7a9c2c185f7b331104649
KashbullinRail <kashbullinrail@gmail.com>
2022-12-02 19:21:47 +0300

    espresso - тест записи полей контакта

--- app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
@@ -2,9 +2,9 @@ package com.example.contactlist
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,6 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Rule
+import kotlin.reflect.typeOf
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -34,6 +35,36 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.fabAddContact))
             .check(matches(isDisplayed()))
             .perform(click())
+
+        val name = "Gevorg"
+
+        onView(withId(R.id.etName))
+            .check(matches(isDisplayed()))
+            .perform(typeText(name))
+            .check(matches(withText(name)))
+
+        val surname = "Gegurgoshvili"
+
+        onView(withId(R.id.etSurname))
+            .check(matches(isDisplayed()))
+            .perform(typeText(surname))
+            .check(matches(withText(surname)))
+
+        val phoneNumber = "34634636"
+
+        onView(withId(R.id.etNumber))
+            .check(matches(isDisplayed()))
+            .perform(typeText(phoneNumber))
+            .check(matches(withText(phoneNumber)))
+
+        onView(withId(R.id.btnSave))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        onView(withId(R.id.rvContacts))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
     }
 
 }
