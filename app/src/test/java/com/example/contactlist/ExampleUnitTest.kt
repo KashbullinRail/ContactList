c591059b6d0c34838f54471ae7eafb0ce6b71d6c
KashbullinRail <kashbullinrail@gmail.com>
2022-12-02 18:04:43 +0300

    подготовка к Unit тестам

--- app/src/test/java/com/example/contactlist/ExampleUnitTest.kt
+++ app/src/test/java/com/example/contactlist/ExampleUnitTest.kt
@@ -14,4 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isFail() {
+        assertNotEquals(4, 5 + 2)
+    }
+
+
 }
