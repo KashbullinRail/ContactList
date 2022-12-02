a708ccd281629fbe7d0e25c9dbcaadad77b0881a
KashbullinRail <kashbullinrail@gmail.com>
2022-12-02 19:07:05 +0300

    espresso - тест нажатия кнопки

--- app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.example.contactlist
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import com.example.contactlist.app.presentation.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +21,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.contactlist", appContext.packageName)
+    fun checkAllComponentIsVisible_Success() {
+        onView(withId(R.id.fabAddContact))
+            .check(matches(isDisplayed()))
+            .perform(click())
     }
+
 }
