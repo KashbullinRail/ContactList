3f493f687f8055585118ce910c01772b13063dab
KashbullinRail <kashbullinrail@gmail.com>
2022-12-03 11:17:27 +0300

    InstrumentalTest - завершил

--- app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
@@ -1,21 +1,17 @@
 package com.example.contactlist
 
-import android.content.Context
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.contactlist.app.presentation.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Assert.*
 import org.junit.Rule
-import kotlin.reflect.typeOf
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -34,35 +30,44 @@ class ExampleInstrumentedTest {
     @Test
     fun checkAllComponentIsVisible_Success() {
 
-        clickOnButton(R.id.fabAddContact)
-        clickOnButton(R.id.btnCancel)
+        clickOn(R.id.fabAddContact)
+        clickOn(R.id.btnCancel)
 
-        clickOnButton(R.id.fabAddContact)
-        writeTextInField(R.id.etName, "Ivan")
-        writeTextInField(R.id.etSurname, "Patanin")
-        writeTextInField(R.id.etNumber, "293423959")
-        clickOnButton(R.id.btnSave)
+        clickOn(R.id.fabAddContact)
+        writeTextInField(R.id.etName, "AAA")
+        writeTextInField(R.id.etSurname, "AAAAA")
+        writeTextInField(R.id.etNumber, "11111111")
+        clickOn(R.id.btnSave)
 
-        clickOnButton(R.id.fabAddContact)
-        writeTextInField(R.id.etName, "Pavel")
-        writeTextInField(R.id.etSurname, "Fedotov")
-        writeTextInField(R.id.etNumber, "37473328")
-        clickOnButton(R.id.btnSave)
+        clickOn(R.id.fabAddContact)
+        writeTextInField(R.id.etName, "BBB")
+        writeTextInField(R.id.etSurname, "BBBBB")
+        writeTextInField(R.id.etNumber, "22222222")
+        clickOn(R.id.btnSave)
 
-        clickOnButton(R.id.fabAddContact)
-        writeTextInField(R.id.etName, "Fedor")
-        writeTextInField(R.id.etSurname, "Karamba")
-        writeTextInField(R.id.etNumber, "43675675")
-        clickOnButton(R.id.btnSave)
+        clickOn(R.id.fabAddContact)
+        writeTextInField(R.id.etName, "CCC")
+        writeTextInField(R.id.etSurname, "CCCCC")
+        writeTextInField(R.id.etNumber, "33333333")
+        clickOn(R.id.btnSave)
 
-        clickOnButton(R.id.rvContacts)
+        clickOn(R.id.rvContacts)
+        clickOn(R.id.btnCancel)
 
+        clickOn(R.id.rvContacts)
+        writeEditTextInField(R.id.etNameEdit, "XX")
+        writeEditTextInField(R.id.etSurnameEdit, "NN")
+        writeEditTextInField(R.id.etNumberEdit, "00")
+        clickOn(R.id.btnSaveEdit)
+
+        clickOn(R.id.rvContacts)
+        clickOn(R.id.btnDelete)
 
     }
 
 }
 
-fun clickOnButton(id: Int) {
+fun clickOn(id: Int) {
     onView(withId(id))
         .check(matches(isDisplayed()))
         .perform(click())
@@ -73,4 +78,10 @@ fun writeTextInField(id: Int, text: String) {
         .check(matches(isDisplayed()))
         .perform(typeText(text))
         .check(matches(withText(text)))
+}
+
+fun writeEditTextInField(id: Int, text: String) {
+    onView(withId(id))
+        .check(matches(isDisplayed()))
+        .perform(typeText(text))
 }
